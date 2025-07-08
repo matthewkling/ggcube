96edd638ae2f371552a4996f08d0529f4515e2ce
Matthew Kling <mattkling@gmail.com>
2025-07-08 11:11:10 -0400

    lighting documentation, mountain simplification

--- data-raw/mountain.R
+++ data-raw/mountain.R
@@ -1,7 +1,14 @@
 ## code to prepare `mountain` dataset
 
 library(raster)
-mountain <- as.data.frame(rasterToPoints(disaggregate(raster(volcano), 2, method = "bilinear")))
-names(mountain) <- c("x", "y", "z")
+library(tidyverse)
+
+mountain <- volcano %>%
+      raster() %>%
+      focal(w = matrix(c(0, 1, 0, 1, 1, 1, 0, 1, 0), 3, 3), fun = mean) %>%
+      aggregate(2, mean) %>%
+      rasterToPoints() %>%
+      as.data.frame() %>%
+      setNames(c("x", "y", "z"))
 
 usethis::use_data(mountain, overwrite = TRUE)
